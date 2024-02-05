bc904e75e539aff46a6d05a6521e0ff286c1596c
Thomas <tlloevkilde@gmail.com>
2024-02-05 09:46:05 +0100

    added console result

--- src/main/java/com/example/dronecalc/DronecalcApplication.java
+++ src/main/java/com/example/dronecalc/DronecalcApplication.java
@@ -6,8 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DronecalcApplication {
 
+	private static int result;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DronecalcApplication.class, args);
+		Calculator calculator = new Calculator();
+
+        result = calculator.add(5, 7);
+        System.out.println("The result is: " + result);
 	}
 
 }
